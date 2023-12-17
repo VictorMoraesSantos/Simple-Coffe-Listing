733e87ab7f89460a2087235a5ca7bcf39a31f001
Victor Moraes <victormoraes2704@gmail.com>
2023-12-17 01:19:52 -0300

    Novas funcionalidades

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const btnAll = document.querySelector('#all');
+const btnAvailable = document.querySelector('#available');
 const coffesContainer = document.querySelector('.coffes-container');
 const url =
   'https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json';
@@ -5,52 +7,79 @@ const url =
 async function coffes() {
   const response = await fetch(url);
   const data = await response.json();
-  data.forEach(
-    ({ id, name, image, price, rating, votes, popular, available }) => {
-      const coffeImg = document.createElement('img');
-      coffeImg.src = image;
-
-      const star = document.createElement('img');
-      star.src = 'img/Star_fill.svg';
-
-      const coffeItem = document.createElement('div');
-      coffeItem.classList.add('coffes-item');
-      coffeItem.appendChild(coffeImg);
-
-      const divPrice = document.createElement('div');
-      divPrice.classList.add('price');
-      const h2 = document.createElement('h2');
-      const span = document.createElement('span');
-
-      h2.textContent = name;
-      span.textContent = price;
-
-      divPrice.appendChild(h2);
-      divPrice.appendChild(span);
-
-      const divRate = document.createElement('div');
-      divRate.classList.add('rate');
-      const img = document.createElement('img');
-      img.src = './img/Star_fill.svg'
-      divRate.appendChild(img);
-
-      const pRate = document.createElement('p');
-      pRate.textContent = rating;
-      divRate.appendChild(pRate);
-
-      const voteSpan = document.createElement('span');
-      voteSpan.textContent = `(${votes} votes)`;
-      if (rating === null) {
-        img.src = './img/Star.svg';
-        voteSpan.textContent  = 'No ratings'
-      }
-      divRate.appendChild(voteSpan);
-
-      coffeItem.appendChild(divPrice);
-      coffeItem.appendChild(divRate);
-
-      coffesContainer.appendChild(coffeItem);
-    }
+  data.forEach(({ name, image, price, rating, votes, popular, available }) =>
+    createElements(name, image, price, rating, votes, popular, available)
   );
+}
+
+function createElements(name, image, price, rating, votes, popular, available) {
+  const coffeImg = document.createElement('img');
+  coffeImg.src = image;
+
+  const star = document.createElement('img');
+  star.src = 'img/Star_fill.svg';
+
+  const coffeItem = document.createElement('div');
+  coffeItem.classList.add('coffes-item');
+  coffeItem.appendChild(coffeImg);
+
+  if (popular === true) {
+    const tagPopular = document.createElement('span');
+    tagPopular.classList.add('popular');
+    tagPopular.textContent = 'Popular';
+    coffeItem.appendChild(tagPopular);
+  }
+
+  const divPrice = document.createElement('div');
+  divPrice.classList.add('price');
+  const h2 = document.createElement('h2');
+  h2.textContent = name;
+  divPrice.appendChild(h2);
+  const span = document.createElement('span');
+  span.textContent = price;
+  divPrice.appendChild(span);
+
+  const divRate = document.createElement('div');
+  divRate.classList.add('rate');
+  const img = document.createElement('img');
+  img.src = './img/Star_fill.svg';
+  divRate.appendChild(img);
+
+  const pRate = document.createElement('p');
+  pRate.classList.add('numRate');
+  pRate.textContent = rating;
+  divRate.appendChild(pRate);
+
+  const voteSpan = document.createElement('span');
+  voteSpan.textContent = `(${votes} votes)`;
+  if (rating === null) {
+    img.src = './img/Star.svg';
+    voteSpan.textContent = 'No ratings';
+  }
+  divRate.appendChild(voteSpan);
+
+  if (available === false) {
+    const soldOut = document.createElement('span');
+    soldOut.classList.add('soldOut');
+    soldOut.textContent = 'Sold out';
+    divRate.appendChild(soldOut);
+  }
+
+  coffesContainer.appendChild(coffeItem);
+  coffeItem.appendChild(divPrice);
+  coffeItem.appendChild(divRate);
+
+  function showAll() {
+    coffeItem.classList.remove('hidden');
+  }
+
+  function showAvailable() {
+    if (available === false) {
+      coffeItem.classList.add('hidden');
+    }
+  }
+
+  btnAll.addEventListener('click', () => showAll());
+  btnAvailable.addEventListener('click', () => showAvailable());
 }
 coffes();
